2c123fd46a32c879e6447ce7f72fd990db804159
Bilobra <bra.carrozzo@gmail.com>
2022-09-21 16:41:48 +0200

    Add links files

---
+++ links.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Links</title>
+</head>
+<body>
+    <ul>
+        <li>
+            <p> <a href="http://localhost/boolean/php-snacks-blocco-1/php-snacks-blocco-1/snacks/snack-1.php">Snack 1</a></p>
+        </li>
+        <li>
+            <p> <a href="http://localhost/boolean/php-snacks-blocco-1/php-snacks-blocco-1/snacks/snack-2.php?name=Bubbi&age=40&email=bubbi2@gmail.com">Snack 2</a></p>
+            
+        </li>
+        <li>
+            <p> <a href="http://localhost/boolean/php-snacks-blocco-1/php-snacks-blocco-1/snacks/snack-3.php">Snack 3</a></p>
+
+        </li>
+        <li>
+            <p> <a href="http://localhost/boolean/php-snacks-blocco-1/php-snacks-blocco-1/snacks/snack-4.php">Snack 4</a></p>
+
+        </li>
+        <li>
+            <p> <a href="http://localhost/boolean/php-snacks-blocco-1/php-snacks-blocco-1/snacks/snack-5.php">Snack 5</a></p>
+
+        </li>
+        <li>
+            <p> <a href="http://localhost/boolean/php-snacks-blocco-1/php-snacks-blocco-1/snacks/snack-6.php">Snack 6</a></p>
+
+        </li>
+        <li>
+            <p> <a href="http://localhost/boolean/php-snacks-blocco-1/php-snacks-blocco-1/snacks/snack-7.php">Snack 7</a></p>
+
+        </li>
+
+    </ul>
+</body>
+</html>
